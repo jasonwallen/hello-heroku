8149b3e285571a5e4b00333eb75e91946a2ad0e1
Jason Allen <jason.allen3@yahoo.com>
2019-08-27 18:38:46 -0400

    sql query commit

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const http = require('http');
+const mysql = require('mysql');
 
 const PORT = process.env.PORT || 5000;
-
+const { JAWSDB_MARIA_IVORY_URL } = process.env;
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+  const client = mysql.createConnection({
+    host      : 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user      : 'qtevfd0vgvvxwf8c',
+    password  : 'qfwuoffbwqrfz31o',
+    port      : '3306',
+    database  : 'g0mdd7886og6dtc0'
+  });
+
+  var sql = "SELECT * FROM hellotable";
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  client.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      res.end('ERROR');
+      client.end();
+    }
+
+    client.query(sql, function(error, results, fields)
+    {
+      if (error) throw error;
+
+      var result_name = results[0].name
+
+      res.end(`${result_name}\n`);
+      client.end();
+    });
+  })
+    // .catch(() => {
+    //   res.end('ERROR');
+    //   client.end();
+    // });
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  // eslint-disable-next-line
+  console.log(`Server running on ${PORT}`);
 });
